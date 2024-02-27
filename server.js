2aee8050e9946517ad3365d44e612c53a359ddff
LampkinSynK <cejajaylin5@gmail.com>
2024-02-27 15:57:49 -0500

    set up register and login endpoint

--- server.js
+++ server.js
@@ -9,24 +9,40 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
-app.get("/api", (req, res) => {
+app.post("/api", (req, res) => {
     res.send({ message: userData });
   });
 
 app.post("/api/login", (req, res) => {
-  const sql = `select * from customer where email='${req.body.email}' and password='${req.body.password}'`
-
+  const sql = `select * from users where Email='${req.body.email}' and userPassword='${req.body.password}'`
+  userData.query(sql, (err, result) => {
+    if (err) throw err;
+    if (result.length > 0) {
+      res.send({ message: "User logged in successfully",
+      user: result[0]});
+    } else {
+      res.send({ message: "User not found" });
+    }
+  });
 });
 
 app.post("/api/register", (req, res) => {
-  const sql = `insert into customer (email, password) values ('${req.body.email}', '${req.body.password}')`;
-  userData.query(sql, (err, result) => {
+  const sql = `insert into users (Email, userPassword, FirstName, LastName, Username, isAdmin) values ('${req.body.email}', '${req.body.password}', '${req.body.firstName}', '${req.body.lastName}', '${req.body.username}', 0)`;
+  const emailCheck = `select * from users where Email='${req.body.email}'`
+  userData.query(emailCheck, (err, result) => {
     if (err) throw err;
-    res.send({ message: "User registered successfully" });
+    if (result.length > 0) {
+      return res.send({ message: "Email already exists" });
+    } else {
+      userData.query(sql, (err, result) => {
+        if (err) throw err;
+        return res.send({ message: "User registered successfully" });
+      });
+    }
   });
 });
 
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
 });
