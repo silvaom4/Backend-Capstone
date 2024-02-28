cd88cf62ef5285adcf15578109f4eed4e3967eae
LampkinSynK <cejajaylin5@gmail.com>
2024-02-28 15:26:50 -0500

    created endpoints for forum

--- server.js
+++ server.js
@@ -42,6 +42,47 @@ app.post("/api/register", (req, res) => {
   });
 });
 
+app.post("/api/forum/posts", (req, res) => {
+  // const sql = `select * from forum`
+  const sql = `select forum.forumID, forum.Content, forum.userID, forum.HeaderContent, users.Username, users.ProfilePicture from forum inner join users on forum.userID = users.UserID`
+  userData.query(sql, (err, result) => {
+    if (err) throw err;
+    return res.send({ message: "Forum posts retrieved successfully", posts: result });
+  });
+});
+
+app.post("/api/forum/post", (req, res) => {
+  const sql = `insert into forum (Content, userID, HeaderContent) values ('${req.body.post}', '${req.body.userID}', '${req.body.header}')`
+  userData.query(sql, (err, result) => {
+    if (err) throw err;
+    return res.send({ message: "Forum post created successfully" });
+  });
+});
+
+app.post("/api/forum/reply", (req, res) => {
+  const sql = `insert into reply (UserID, forumID, Content) values ('${req.body.userID}', '${req.body.postID}', "${req.body.reply}")`
+  userData.query(sql, (err, result) => {
+    if (err) throw err;
+    return res.send({ message: "Forum reply created successfully" });
+  });
+});
+
+app.post("/api/forum/replies", (req, res) => {
+  const sql = `select * from reply where forumID='${req.body.postID}'`
+  userData.query(sql, (err, result) => {
+    if (err) throw err;
+    return res.send({ message: "Forum replies retrieved successfully", replies: result });
+  });
+});
+
+app.post("/api/forum/delete", (req, res) => {
+  const sql = `delete from forum where PostID='${req.body.postID}'`
+  userData.query(sql, (err, result) => {
+    if (err) throw err;
+    return res.send({ message: "Forum post deleted successfully" });
+  });
+});
+
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
